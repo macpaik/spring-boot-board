6d441e19832edcbd7d6a394d5f5f7dca106f1bcd
Paik-yoonsuk <paik@metaps-payment.com>
2020-02-16 21:13:47 +0900

    add write

--- src/main/java/com/macxpaik/springbootboard/SpringBootBoardApplication.java
+++ src/main/java/com/macxpaik/springbootboard/SpringBootBoardApplication.java
@@ -2,13 +2,15 @@ package com.macxpaik.springbootboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootBoardApplication {
 
     public static void main(String[] args) {
-        System.setProperty("spring.devtolls.restart.enabled", "false");
-        System.setProperty("spring.devtolls.livereload.enabled", "true");
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        System.setProperty("spring.devtools.livereload.enabled", "true");
         SpringApplication.run(SpringBootBoardApplication.class, args);
     }
 
