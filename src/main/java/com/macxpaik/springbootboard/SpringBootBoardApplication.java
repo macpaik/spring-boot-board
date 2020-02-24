21f06c792917cdc9fc73243b3124b65d148bf3db
macpaik <macxpaik@gmail.com>
2020-02-24 18:05:52 +0900

    hiddenHttpMethod設定

--- src/main/java/com/macxpaik/springbootboard/SpringBootBoardApplication.java
+++ src/main/java/com/macxpaik/springbootboard/SpringBootBoardApplication.java
@@ -2,7 +2,12 @@ package com.macxpaik.springbootboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +15,13 @@ public class SpringBootBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootBoardApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean hiddenHttpMethodFilter() {
+        FilterRegistrationBean filterRegBean = new FilterRegistrationBean(new HiddenHttpMethodFilter());
+        filterRegBean.setUrlPatterns(Arrays.asList("/*"));
+        return filterRegBean;
     }
 
 }
